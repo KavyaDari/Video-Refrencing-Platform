f425594f2d4b1942a76cdd6da1ac8c5fc7bdab75
Kavya Dari <146530664+KavyaDari@users.noreply.github.com>
2026-07-26 23:47:23 +0530

    Change BACKEND_CORS_ORIGINS to use type hinting

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,11 +4,12 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Zoom Clone"
     DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./zoom.db")
-    BACKEND_CORS_ORIGINS = [
+
+    BACKEND_CORS_ORIGINS: list[str] = [
         "http://localhost:3000",
         "https://video-refrencing-platform.vercel.app",
     ]
-    
+
     class Config:
         case_sensitive = True
         env_file = ".env"
